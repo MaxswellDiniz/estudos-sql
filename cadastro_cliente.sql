522cfa363d56bfbe0bfb19eb9005f70ca9b7cbec
Maxswell.Diniz <maxswellsousadiniz@gmail.com>
2026-09-03 18:15:05 -0300

    Ajustando dados de teste de cadastro de cliente

--- cadastro_cliente.sql
+++ cadastro_cliente.sql
@@ -18,7 +18,7 @@ CREATE TABLE CLIENTES(
 
 -- 2. Inserção de dados de teste (Dados Sintéticos)
 
-INSERT INTO CLIENTES (nome,sobrenome,email,telefone) VALUES ('Max','Diniz','maxswellsousadiniz@yahoo.com','(11) 946742182')
+INSERT INTO CLIENTES (nome,sobrenome,email,telefone) VALUES ('SeuNome','SeuSobrenome','SeuEmail','(dd) 99999999')
 
 -- 3. Consulta de Verificação
 
